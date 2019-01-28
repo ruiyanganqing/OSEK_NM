e33f0d8157380623db52238e5767882c0fe1cdbb
ys <1175818152@qq.com>
2019-01-28 17:36:33 +0800

    update

--- App/keil/Objects/oseknmserver.d
+++
@@ -1,3 +0,0 @@
-.\objects\oseknmserver.o: ..\..\OsekNM_core\OsekNMServer.c
-.\objects\oseknmserver.o: ..\..\OsekNM_core\OsekNMServer.h
-.\objects\oseknmserver.o: ..\..\OsekNM_core\OSEKNM.h
--- App/keil/Objects/startup_stm32f40_41xxx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f40_41xxx.o: ..\..\Driver\stm32f407\CORE\startup_stm32f40_41xxx.s
